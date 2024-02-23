aadb17ee37c59d40fd2706952fa83e36bbff8fe4
ymkt1101 <150763897+ymkt1101@users.noreply.github.com>
2024-02-24 01:13:30 +0900

    PF進捗報告2回目

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -10,28 +10,71 @@
   })(document);
 
 // ローディング 
-$(function(){
-      $('.ld').addClass('is-active');
-  setTimeout(function() {
-    $('.ld').fadeOut();
-  }, 6000);
+jQuery(function () {
+  var webStorage = function () {
+    if (sessionStorage.getItem('access')) {
+      //2回目以降アクセス時の処理
+      console.log('2回目以降のアクセスです');
+      $(".loading").addClass('is-active');
+      
+    } else {
+      //初回アクセス時の処理
+      sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
+      $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
+      setTimeout(function () {
+        // ローディングを数秒後に非表示にする
+        $(".loading").addClass('is-active');
+        $(".loading-animation").removeClass('is-active');
+      }, 7000); // ローディングを表示する時間
+    }
+  }
+  webStorage();
+ 
 });
 
 // ハンバーガーメニュ
-$(".ham_btn").click(function () {
+$(".nv_btn").click(function () {
   $(this).toggleClass('active');
 });
 
-// カルーセル
-$('.slider').slick({
-  autoplay: true,//自動的に動き出すか。初期値はfalse。
-    autospeed: 5000,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    centerMode: true,//要素を中央ぞろえにする
-    variableWidth: true,//幅の違う画像の高さを揃えて表示
-  dots: false,//下部ドットナビゲーションの表示
+// カルーセルとリサイズ時
+function initializeSlick() {
+  var windowWidth = $(window).width();
+  var slidesToShow = (windowWidth < 768) ? 1 : 3;
+
+  $('.slider').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    dots: false,
+  });
+}
+
+initializeSlick();
+
+$(window).on('resize', function() {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    var windowWidth = $(window).width();
+
+    if (windowWidth < 768) {
+      $('.slider').slick('unslick');
+    } else {
+      initializeSlick();
+    }
+  }, 250);
 });
-    
+
+// モーダル
+$(function () {
+  $('#openModal').click(function(){
+      $('#modalArea').fadeIn();
+  });
+  $('#closeModal , #modalBg').click(function(){
+    $('#modalArea').fadeOut();
+  });
+});
