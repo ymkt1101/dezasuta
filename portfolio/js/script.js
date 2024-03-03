791b9e86e6242312b838974693f6ca0efe9c2a32
ymkt1101 <150763897+ymkt1101@users.noreply.github.com>
2024-03-04 02:07:42 +0900

    PF進捗報告4回目

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -10,11 +10,10 @@
   })(document);
 
 // ローディング 
-jQuery(function () {
+$(function () {
   var webStorage = function () {
     if (sessionStorage.getItem('access')) {
       //2回目以降アクセス時の処理
-      console.log('2回目以降のアクセスです');
       $(".loading").addClass('is-active');
       
     } else {
@@ -22,10 +21,10 @@ jQuery(function () {
       sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
       $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
       setTimeout(function () {
-        // ローディングを数秒後に非表示にする
         $(".loading").addClass('is-active');
+        // ローディングを指定時間表示してクラスを外す
         $(".loading-animation").removeClass('is-active');
-      }, 7000); // ローディングを表示する時間
+      }, 7000); 
     }
   }
   webStorage();
@@ -54,16 +53,15 @@ $(function(){
 function initializeSlick() {
   var windowWidth = $(window).width();
   var slidesToShow = (windowWidth < 768) ? 1 : 3;
-
-  $('.slider').slick({
+  
+  // $('.slider').slick({
+    $('.slider').not('.slick-initialized').slick ({
     infinite: true,
     autoplay: true,
     autoplaySpeed: 5000,
     slidesToShow: slidesToShow,
-    // slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: '25vw',
     variableWidth: true,
     dots: false,
   });
@@ -72,24 +70,57 @@ function initializeSlick() {
 initializeSlick();
 
 $(window).on('resize', function() {
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(function() {
-    var windowWidth = $(window).width();
-
-    if (windowWidth < 768) {
-      $('.slider').slick('unslick');
-    } else {
-      initializeSlick();
+    var elements = document.getElementsByClassName("skill");
+    var windowWidth = window.innerWidth;
+    
+    initializeSlick();
+  
+  if (windowWidth > 480 && windowWidth < 660) {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.width = "480px";
     }
-  }, 250);
+      // $('.slider').slick('unslick');
+    } else {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.width = "fit-content";
+    }
+    }
+    // var resizeTimer;
+    
+    // clearTimeout(resizeTimer);
+    // resizeTimer = setTimeout(function() {
+    //   var windowWidth = $(window).width();
+    //   if (windowWidth > 500 && windowWidth < 640) {
+    //     $('.slider').slick('unslick');
+    //   } else {
+    //     initializeSlick();
+    //   }
+    // }, 250);
 });
 
 // モーダル
-$(function () {
-  $('#openModal').click(function(){
-      $('#modalArea').fadeIn();
-  });
-  $('#closeModal , #modalBg').click(function(){
-    $('#modalArea').fadeOut();
-  });
+$('.slick-thumbnail').on('click', function() {
+  var modal = $('#wk_modal');
+  var modalImg = $('#wk_modalImage');
+  var modalDescription1 = $('#modalDescription1');
+  var modalDescription2 = $('#modalDescription2');
+
+  // クリックされたサムネイルのdata-large属性と説明文を取得してモーダルに表示
+  var largeSrc = $(this).data('large');
+  var description1 = document.querySelector('.wk_mdl_txt1').textContent;
+  var description2 = document.querySelector('.wk_mdl_txt2').textContent;
+
+  modalDescription1.text(description1);
+  modalDescription2.text(description2);
+  modalImg.attr('src', largeSrc);
+  modal.css('display', 'block');
+
+  $('body').addClass('scrolllock');
+});
+  
+// モーダルを閉じる
+$('.wk_mdl_close, .wk_mdl').on('click', function() {
+  $('#wk_modal').css('display', 'none');
+  $('body').removeClass('scrolllock');
+  
 });
